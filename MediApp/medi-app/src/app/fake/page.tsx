1f082bcd96bb83567034f05d7e1ced262215f95e
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2025-03-14 22:46:43 -0300

    update_medi_app

--- MediApp/medi-app/src/app/fake/page.tsx
+++ MediApp/medi-app/src/app/fake/page.tsx
@@ -1,36 +1,67 @@
-"use client"
+"use client";
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
+
+// Defina a interface para os dados da API
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+}
 
 export default function FakeApi() {
+  // Defina o tipo do estado como User[] ou null
+  const [fakeApiData, setFakeApiData] = useState<User[] | null>(null);
 
-    const [fakeApiData, setFakeApiData] = useState(null);
-
-    const requestFakeApi = async () => {
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            setFakeApiData(response.data);
-        } catch (e) {
-            console.log(e);
-        }
-
+  const requestFakeApi = async () => {
+    try {
+      const response = await axios.get<User[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setFakeApiData(response.data); 
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    return (
-        <>
-            <h1>Dados Fake Api</h1>
-            <button className="bg-green-500 p-2 inline-block text-white text-sm"
-             onClick={(e) => requestFakeApi()}>Buscar dados Fake Api</button>
-            <table>
-                {!!fakeApiData && fakeApiData.map((data: any) => (
-                    <tr>
-                        <td className='border border-slate-300'>{data.name}</td>
-                        <td className='border border-slate-300 text-center'>{data.username}</td>
-                        <td className='border border-slate-300 text-center'>{data.email}</td>
-                        <td className='border border-slate-300 text-center'>{data.phone}</td>
-                    </tr>
-                ))}
-            </table>
-        </>
-    )
+  return (
+    <>
+      <h1>Dados Fake Api</h1>
+      <button
+        className="bg-green-500 p-2 inline-block text-white text-sm"
+        onClick={requestFakeApi}
+      >
+        Buscar dados Fake Api
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Telefone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fakeApiData && 
+            fakeApiData.map((data) => (
+              <tr key={data.id}>
+                <td className="border border-slate-300">{data.name}</td>
+                <td className="border border-slate-300 text-center">
+                  {data.username}
+                </td>
+                <td className="border border-slate-300 text-center">
+                  {data.email}
+                </td>
+                <td className="border border-slate-300 text-center">
+                  {data.phone}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
--- MediApp/medi-app/src/app/layout.tsx
+++ MediApp/medi-app/src/app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import Header from "./components/Headers";
-import './globals.css'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Medi App',
-  description: 'Medical application',
-}
+  title: "Medi App",
+  description: "Medical application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className='w-full max-w-7xl mt-4 m-auto'>
-            {children}
-        </div>
+        <div className="w-full max-w-7xl mt-4 m-auto">{children}</div>
       </body>
     </html>
-  )
+  );
 }
